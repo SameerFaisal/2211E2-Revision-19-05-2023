75709d6325a89c74f45b8067b167c5a600e8c597
SameerFaisal <sameerfaisal46@gmail.com>
2023-05-19 16:58:46 +0500

    Revision 2211E2 19-05-2023

---
+++ app.js
@@ -0,0 +1,96 @@
+//1. Primitive Data Types
+// let data=12  //Integer
+// let float=12.657  //Float
+// let str="Sameer"  //String
+// let bool=true  //Boolean
+
+//2. Composite Data Types
+// let arr=[1, 2, 3, 4, 5]  //Array
+// let obj={name:"Sameer", age:25}  //Object
+
+//3. Printing Techniques
+// alert("Hello World")
+// document.write("Hello World")
+// console.log("Hello World")
+// document.write("Sameer")
+
+//4. Printing Variables
+// console.log(data)
+// console.log(float)
+// console.log(str)
+// console.log(bool)
+// console.log("data")
+// console.log(data)
+
+//5. Arithmetic Operators (+, -, *, /, %)
+//Number+String, String+Number, String+String, String+Boolean, Boolean+string, Number+Boolean, Boolean+Number //Concatenation
+
+// let num1=5
+// let num2="two"
+// let result=num1+num2
+// console.log(result)
+
+// let num1=5
+// let num2="10"
+// let result=num1+num2
+// alert(result)
+
+// let n1="ten"
+// let n2=5
+// let result=n1-n2
+// console.log(result)
+
+// let n1="TEN"
+// let n2=5
+// let result=n1*n2
+// console.log(result)
+
+// let n1=10
+// let n2=5
+// let result=n1/n2
+// console.log(result)
+
+// let n1="ten"
+// let n2=3
+// let result=n1%n2
+// console.log(result)
+
+// let n=10
+// let m=false
+// let result=m+n
+// console.log(result)
+
+// let n="Hadi"
+// let m=false
+// let result=m+n
+// console.log(result)
+
+// let expression=5*3/5+9-10
+// console.log(expression)
+// //student expected answer = 7.3 
+// //actual answer = 2
+
+// let value=5*3/5+(9-10)
+// 5*3/5-1
+// 5*0.6-1
+// 3-1
+// 2
+// console.log(value)
+//expected answer = -2.5
+//actual answer = 2
+
+// let expression=(5*3)/(5+(9-10))
+// console.log(expression)
+//expected answer = 3.75
+//actual answer = 3.75
+
+//6. User Input (prompt)
+
+// let user=parseFloat(prompt("Enter your name"))
+// console.log(user)
+// console.log(typeof(user))
+
+// let fname=prompt("Enter your first name")
+// let lname=prompt("Enter your last name")
+
+// alert(fname+" "+lname)
